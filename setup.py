2cb4ca3cd5731de23bdb4f0382e6b45664bb9d5b
Meryem MERHOUNI <meryemmerhouni@gmail.com>
2025-11-02 00:54:11 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="meryem",
+    author_email="meryem.merhouni@g.enp.edu.dz",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
